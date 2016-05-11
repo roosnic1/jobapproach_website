3ecc222698a46feb739d010b1123aed5c021df1f
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-05-11 11:31:27 +0200

    minor changes

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import autobind from 'autobind-decorator';
+import About from './About';
 
 class App extends React.Component {
 
 	render() {
 		return (
 			<div className="app">
-				<h1>JobApproach</h1>
+				<div className="row center-xs">
+					<div className="col-xs-12">
+						<h1>JobApproach</h1>
+					</div>
+				</div>
+				<div className="row">
+				</div>
+				<div className="row">
+					<About />
+				</div>
 			</div>
 		);
 	}
