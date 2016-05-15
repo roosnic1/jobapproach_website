676a375d310355ec6bcfa6eaccb1d04970db7b45
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-05-15 12:05:07 +0200

    updated php sendmail with google recaptcha verification

--- app/sendmail.php
+++ app/sendmail.php
@@ -1,10 +1,23 @@
 <?php
 
-$email=$_POST['email'];
-$message=json_encode($_POST);
-$receiver="info@jobapproach.ch";
-$mailer="contact-form@jobapproach.ch";
+require('recaptcha/autoload.php');
+$secret = file_get_contents('./recapSecret.txt');
 
-mail($receiver,"Contact",$message,  "From:" . $mailer);
+$gRecaptchaResponse = $_POST['recaptcha'];
+$remoteIp = $_SERVER['REMOTE_ADDR'];
+
+$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+$resp = $recaptcha->verify($gRecaptchaResponse, $remoteIp);
+if ($resp->isSuccess()) {
+    $_POST['recaptcha'] = '';
+    $message=json_encode($_POST);
+    $receiver="info@jobapproach.ch";
+    $mailer="contact-form@jobapproach.ch";
+
+    mail($receiver,"Contact",$message,  "From:" . $mailer);
+} else {
+    $errors = $resp->getErrorCodes();
+    // echo json_encode($errors);
+}
 
 ?>
--- gulpfile.js
+++ gulpfile.js
@@ -88,6 +88,7 @@ gulp.task('fonts', () => {
 gulp.task('extras', () => {
   return gulp.src([
     'app/*.*',
+    'app/**/*.php',
     '!app/*.html'
   ], {
     dot: true
